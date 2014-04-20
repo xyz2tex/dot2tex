3cd6f6f2e75c9c45193a8fdb31df2c09b4d2f3cd
kjellmf <kjellmf@gmail.com>
2014-04-20 18:23:05 +0200

    Update copyright year

--- doc/conf.py
+++ doc/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'dot2tex'
-copyright = u'2011, Kjell Magne Fauske'
+copyright = u'2014, Kjell Magne Fauske'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
--- dot2tex/dot2tex.py
+++ dot2tex/dot2tex.py
@@ -8,7 +8,7 @@ Copyright (c) 2006-2014, Kjell Magne Fauske
 
 """
 
-# Copyright (c) 2006-2009, Kjell Magne Fauske
+# Copyright (c) 2006-2014, Kjell Magne Fauske
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to
