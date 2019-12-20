60b0c672adfb7571bdc68a894f4b07d31ff0ae87
Kjell Magne Fauske <kjellmf@gmail.com>
2019-12-20 18:06:20 +0100

    Make setuptools a strong dependency
    Installation will not work if we use distutils.
    Resolves #75

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
 
 setup(name='dot2tex',
       version='2.12.dev',
